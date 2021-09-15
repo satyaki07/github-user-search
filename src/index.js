48dcebba4a18aedc3ce9280088cff4651fad8bb1
unknown <satyakibose98@gmail.com>
2021-09-15 20:46:43 +0530

    feat: add login/logout - auth0

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+//dev-eg5q018n.us.auth0.com
+//vnfwPToSYklJeYvkHz0gQo01uYM5yvv9
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-eg5q018n.us.auth0.com"
+      clientId="vnfwPToSYklJeYvkHz0gQo01uYM5yvv9"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
